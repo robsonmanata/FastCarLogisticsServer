6069229650affa2d688f9da0b10734af71c7faa9
robsonmanata <your_email@example.com>
2025-12-02 01:31:03 +0100

    gitignore

--- controllers/orders.js
+++ controllers/orders.js
@@ -4,7 +4,7 @@ import Product from '../models/products.js';
 
 export const getOrders = async (req, res) => {
     try {
-        const orders = await Order.find();
+        const orders = await Order.find().sort({ _id: -1 });
         res.status(200).json(orders);
     } catch (error) {
         res.status(404).json({ message: error.message });
